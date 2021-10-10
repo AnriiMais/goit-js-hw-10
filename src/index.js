eb7a6e4e8b3cf1ea0b11704366009651e2a507bb
Andrii Maistrenko <maystrenko22a@gmail.com>
2021-10-10 19:44:24 +0300

    correcrion

--- src/index.js
+++ src/index.js
@@ -4,38 +4,6 @@ import debounce from 'lodash.debounce';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 500;
-// example
-// const fetchUsersBtn = document.querySelector('.btn');
-// const userList = document.querySelector('.user-list');
-
-// fetchUsersBtn.addEventListener('click', () => {
-//   fetchUsers()
-//     .then(users => renderUserList(users))
-//     .catch(error => console.log(error));
-// });
-
-// function fetchUsers() {
-//   return fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
-
-// function renderUserList(users) {
-//   const markup = users
-//     .map(user => {
-//       return `<li>
-//           <p><b>Name</b>: ${user.name}</p>
-//           <p><b>Email</b>: ${user.email}</p>
-//           <p><b>Company</b>: ${user.company.name}</p>
-//         </li>`;
-//     })
-//     .join('');
-//   userList.innerHTML = markup;
-// }
-// -/example
 const refs = {
   inputCountryName: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
